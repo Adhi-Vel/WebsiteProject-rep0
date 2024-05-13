8107deaa727cc0f8c9df32e8bbb29e64ce6c24c6
Adhithya Vel M V <adhithya.vel7@gmail.com>
2024-05-13 19:36:15 +0100

    Final Project Submission

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-  var donateButton = document.querySelector(".Submit");
+// Select the submit button element
+var donateButton = document.querySelector(".Submit");
 
-  donateButton.addEventListener("click", function(event) {
-    event.preventDefault(); 
-    var fullName = document.querySelector('input[name="fullname"]').value;
-    var email = document.querySelector('input[name="email"]').value;
-    var address = document.querySelector('input[name="address"]').value;
-    var city = document.querySelector('input[name="city"]').value;
-    var state = document.querySelector('input[name="state"]').value;
-    var country = document.querySelector('input[name="country"]').value;
-    var postcode = document.querySelector('input[name="postcode"]').value;
-    var cardName = document.querySelector('input[name="cardname"]').value;
-    var cardNumber = document.querySelector('input[name="cardnumber"]').value;
-    var expMonth = document.querySelector('input[name="expmonth"]').value;
-    var expYear = document.querySelector('input[name="expyear"]').value;
-    var cvv = document.querySelector('input[name="cvv"]').value;
-    alert("Thank you " + fullName + ", for your donation!");  
-  });
-//
+// Add an event listener for the click event on the submit button
+donateButton.addEventListener("click", function(event) {
+  event.preventDefault(); // Prevent the default form submission behavior
+
+  // Retrieve values from input fields
+  var fullName = document.querySelector('input[name="fullname"]').value;
+  var email = document.querySelector('input[name="email"]').value;
+  var address = document.querySelector('input[name="address"]').value;
+  var city = document.querySelector('input[name="city"]').value;
+  var state = document.querySelector('input[name="state"]').value;
+  var country = document.querySelector('input[name="country"]').value;
+  var postcode = document.querySelector('input[name="postcode"]').value;
+  var cardName = document.querySelector('input[name="cardname"]').value;
+  var cardNumber = document.querySelector('input[name="cardnumber"]').value;
+  var expMonth = document.querySelector('input[name="expmonth"]').value;
+  var expYear = document.querySelector('input[name="expyear"]').value;
+  var cvv = document.querySelector('input[name="cvv"]').value;
+
+  // Display a thank you message with the donor's name
+  alert("Thank you " + fullName + ", for your donation!");  
+});
